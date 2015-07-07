a32171e0a314dea1c4d9d970f83a486ece3b1e00
denilee <DeniLiR@gmail.com>
2015-07-07 11:42:33 -0700

    what Java file

---
+++ hello.java
@@ -0,0 +1,19 @@
+
+class WpromoteEmployee{
+    private int age;
+    private String name;
+    public WpromoteEmployee(){
+
+    }
+    public WpromoteEmployee(int age,String name){
+      this.age = age;
+      this.name = name;
+    }
+    public void getEmployeeInformation(){
+        System.out.println("Age: "+this.age+" Name: "+this.name);
+    }
+
+    public String getFoo(){
+        return "foo";
+    }
+}
